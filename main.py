b9c1c00590c3e53dd9eb92a1e9491b636a639383
Josefina Baro <j_barofranco@mail.fhsu.edu>
2024-09-12 17:27:20 -0500

    Set up yfinance to pull 5 tickers, snap, googl, lyft, roku, meta

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 ## NF601 - Advanced Programming in Python
 ## Josefina Baro
 ## Mini Project 1
+
+import pprint
+import yfinance as yf
+
+mytickers = ["SNAP" , "GOOGL" , "LYFT" , "ROKU" , "META"]
+mytickers.sort()
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    print(f"Ticker: {ticker} \tDay High: {result.info['dayHigh']}")
