e1efd3eb8b2c38f026c36042d13be0c82e65271a
Josefina Baro <j_barofranco@mail.fhsu.edu>
2024-09-18 14:48:43 -0500

    changed # of days, fine tuned plots (max & mins)

--- main.py
+++ main.py
@@ -4,9 +4,12 @@
 
 import pprint
 import yfinance as yf
+from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
+today = datetime.now()
 ##(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 ##(5/5 points) Proper import of packages used.
 ##(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
@@ -17,23 +20,24 @@ import matplotlib.pyplot as plt
 ##(10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 ##(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
 
+ten_days_ago = today - timedelta(days=15)
 mytickers = ["SNAP" , "GOOGL" , "LYFT" , "ROKU" , "META"]
 mytickers.sort()
 
 for ticker in mytickers:
     result = yf.Ticker(ticker)
-    history = result.history(period="5d")
+    history = result.history(start = ten_days_ago, end = datetime.now())
 
     closingList = []
 
-    for price in history["Close"]:
-        closingList.append(price)
-    print(closingList)
-
+    for date in history["Close"][:11]:
+        closingList.append(date)
     stockarray = np.array(closingList)
-
+    maxprice = stockarray.max() + (stockarray.max()*.05)
+    minprice = stockarray.min() - (stockarray.min() * .05)
     plt.plot(stockarray)
-    plt.title(f"{ticker} Stock Closing Prices")
-    plt.xlabel("Days")
+    plt.title(f"{ticker} Stock Closing Price - Last 10 Days")
+    plt.xlabel("Days Ago")
     plt.ylabel("Closing Price")
+    plt.axis((9,0, minprice, maxprice))
     plt.show()
