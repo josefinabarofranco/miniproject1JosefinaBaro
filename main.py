cd8795c951d936c3fbafca93640b9fab782dd61d
Josefina Baro <j_barofranco@mail.fhsu.edu>
2024-09-18 15:20:36 -0500

    final edits README acknowledgments

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 os.makedirs("charts", exist_ok=True)
 
 today = datetime.now()
+
 ##(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 ##(5/5 points) Proper import of packages used.
 ##(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
